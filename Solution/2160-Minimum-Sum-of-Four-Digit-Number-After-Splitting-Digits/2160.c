12bea72a45818f7b0cf7f78d306479381e26594c
CalvinWan0101 <t110820058@ntut.org.tw>
2022-02-10 00:15:49 +0800

    2160

---
+++ Solution/2160-Minimum-Sum-of-Four-Digit-Number-After-Splitting-Digits/2160.c
@@ -0,0 +1,18 @@
+int minimumSum(int num)
+{
+    int digits[4];
+    for (int i = 0; i < 4; i++)
+    {
+        digits[i] = num % 10;
+        num /= 10;
+    }
+    for (int i = 0; i < 3; i++)
+        for (int j = i + 1; j < 4; j++)
+            if (digits[i] > digits[j])
+            {
+                int temp = digits[i];
+                digits[i] = digits[j];
+                digits[j] = temp;
+            }
+    return (digits[0] + digits[1]) * 10 + digits[2] + digits[3];
+}
