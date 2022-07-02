35f59eb157063161990e6f7d85937dc36212a46e
CalvinWan0101 <t110820058@ntut.org.tw>
2022-07-03 01:42:29 +0800

    0009

---
+++ Solution/0009-Palindrome-Number/0009.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        long long int after = 0, before = x;
+
+        while (x)
+        {
+            after = after * 10 + x % 10;
+            x /= 10;
+        }
+
+        return after == before;
+    }
+};
